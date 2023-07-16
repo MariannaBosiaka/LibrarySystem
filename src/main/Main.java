441f7fd12ab45bcf5a001daae3d5dd82c0093bbd
Marianna Bosiaka <57630657+MariannaBosiaka@users.noreply.github.com>
2023-07-16 09:22:42 +0300

    created packages and moved main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,8 @@
+package main;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
