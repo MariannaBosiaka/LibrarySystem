a53d7cc98399bfecaa6a08c8041c7a65c716c046
Marianna Bosiaka <57630657+MariannaBosiaka@users.noreply.github.com>
2023-07-16 09:18:06 +0300

    connection to database

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
-    
+    public static void main(String[] args) throws SQLException {
+        new Main();
+    }
+
+    public Main() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/librarysys", "root", "m54#!br34A");
+        System.out.println("Successfully connected to database");
+    }
 }
