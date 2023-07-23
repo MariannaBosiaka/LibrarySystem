91b339a2543309b27f08bd5f9e760fbdea2bd56b
Marianna Bosiaka <57630657+MariannaBosiaka@users.noreply.github.com>
2023-07-23 16:47:25 +0300

    centered and made window movable

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,15 +2,20 @@ package main;
 
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import utils.DataBaseUtils;
 
 public class Main extends Application {
 
+    private double xOffset, yOffset;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -19,7 +24,22 @@ public class Main extends Application {
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.initStyle(StageStyle.UNDECORATED); //to not show the title bar
+
+        //to be able to move the window around
+        root.setOnMousePressed(pressEvent -> {
+            root.setOnMouseDragged(dragEvent -> {
+                primaryStage.setX(dragEvent.getScreenX() - pressEvent.getSceneX());
+                primaryStage.setY(dragEvent.getScreenY() - pressEvent.getSceneY());
+            });
+        });
+        
         primaryStage.show();
+
+        //3 lines of code to center the javafx window - !!In order to work UNDER THE .show() method!!
+        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+        primaryStage.setX((primScreenBounds.getWidth() - primaryStage.getWidth()) / 2);
+        primaryStage.setY((primScreenBounds.getHeight() - primaryStage.getHeight()) / 2);
+
     }
 
 
